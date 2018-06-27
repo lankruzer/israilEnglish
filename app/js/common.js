192bef44d58eee226e6bdd9a30127a30e72c4fbd
lan_kruzer <ar-48@yandex,ru>
2018-06-28 01:46:57 +0300

    add page pricing

--- app/js/common.js
+++ app/js/common.js
@@ -24,3 +24,38 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	// mobile menu
 	
 })
+
+// tabs
+if (document.querySelector('.period-tabs-nav')) {
+	var tabsNavContainer = $('.period-tabs-nav');
+	var tabsNavButtons = $('.period-tabs-nav-li');
+	var tabsCurrentIndex = $(tabsNavContainer).find('.is-active').index('.period-tabs-nav-li');
+	var tabsContent = $('.period-tabs-content');
+
+
+	$(tabsNavButtons).click(function() {
+		if (!$(this).hasClass('is-active')) {
+			$(tabsNavButtons).removeClass('is-active');
+			$(this).addClass('is-active');
+			tabsCurrentIndex = $(this).index('.period-tabs-nav-li');
+
+			$(tabsContent).each(function(idnex, item) {
+				var tabsContentItems = $(item).find('.period-tabs-content-li');	
+				$(tabsContentItems).removeClass('is-show');
+				$($(tabsContentItems[tabsCurrentIndex])).addClass('is-show');
+			})
+		}
+	})
+}
+
+// faq
+if (document.querySelector('.faq-list')) {
+	$('.faq-list-question').click(function(){
+		if (!$(this).hasClass('is-active')) {
+			$('.faq-list-question').not($(this)).removeClass('is-active');
+			$('.faq-list__answer').not($(this).next()).slideUp(250);
+			$(this).addClass('is-active');
+			$($(this).next()).slideToggle(250);
+		}
+	})
+}
